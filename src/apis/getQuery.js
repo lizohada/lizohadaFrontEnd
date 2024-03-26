215f722876231428746b2679b798230de5770550
ohchansol <haxr369@gmail.com>
2024-03-26 11:00:12 +0900

    Feat / getQuery 더미 데이터로 질문지 받고 쿼리스트링에 적용 완료

---
+++ src/apis/getQuery.js
@@ -0,0 +1,20 @@
+// import axios from "axios";
+
+export default async function getQuery() {
+  const tempJson = {
+    query: [
+      ["바다", "밥"],
+      ["오름", "산"],
+      ["박물과", "해변"],
+      ["소나무", "서핑"],
+      ["배", "턱걸이"],
+      ["딸기축제", "낙엽"],
+    ],
+  };
+  try {
+    // const response = await axios.get("YOUR_API_ENDPOINT");
+    return tempJson;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
--- src/game/Game.js
+++ src/game/Game.js
@@ -1,32 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ImageButton from "../components/ImageButton";
-export default function Game() {
-  const [taste] = useState("");
+import getQuery from "../apis/getQuery";
 
+export default function Game() {
+  const [query, setQuery] = useState([]);
   // 클릭할 때마다 쿼리스트링에 배열 데이터를 추가하는 함수
-  function handleAddTaste(taste) {
+  function handleAddTaste(key) {
     const params = new URLSearchParams();
-    params.set("taste", taste);
+    params.set("key", key);
     const queryString = params.toString();
     window.history.pushState(null, "", `?${queryString}`);
   }
 
+  // 쿼리 데이터를 받아옵니다.
+  useEffect(() => {
+    getQuery().then((json) => {
+      setQuery(json.query);
+    });
+  }, []);
+  function CreateImgComponent(array) {
+    console.log(array.array);
+    array.array.map((element, index) => console.log(element));
+    return (
+      <>
+        {array.array.map((element, index) => (
+          <div className="max-w-24rem">
+            <ImageButton
+              handler={handleAddTaste}
+              keyword={element}
+              srcLink={"/logo.png"}
+            />
+            <div className="py-4 px-4" />
+          </div>
+        ))}
+      </>
+    );
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="max-w-24rem">
-        <ImageButton
-          handler={handleAddTaste}
-          taste={taste}
-          srcLink={"/vercel.svg"}
-        />
-      </div>
-      <div className="max-w-24rem">
-        <ImageButton
-          handler={handleAddTaste}
-          taste={taste}
-          srcLink={"/logo.png"}
-        />
-      </div>
+      {query.length === 0 ? (
+        <div>질문지 받아오는 중</div>
+      ) : (
+        <CreateImgComponent array={query[0]} />
+      )}
     </div>
   );
 }
